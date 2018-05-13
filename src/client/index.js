b65e7d96ad395679e561fea68987f2d90ffd69f1
Abel <abelcd@protonmail.com>
2018-05-13 13:38:42 +0200

    fix script not found

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,6 @@
 import path from "path"
 
-export const publicOutput = path.join(__dirname, "../.dist/browser/public")
-export const adminOutput = path.join(__dirname, "../.dist/browser/admin")
+export const publicOutput = path.join(__dirname, "../../.dist/browser/public")
+export const adminOutput = path.join(__dirname, "../../.dist/browser/admin")
 
 export default {publicOutput, adminOutput}
